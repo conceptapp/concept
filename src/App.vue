aa22eaf4561587a0fd227ea03d2602fc6c7e6650
Jérémie Croyère <jcroyere@linxea.com>
2019-03-05 16:55:02 +0100

    Added query to Airtable
    with Axios
    to Concept table

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
     <img style="width:150px" src="@/assets/logo.png" />
-    <router-view />
+    <div class="row">
+      <ul>
+        <li v-for="item in items" :key="trick.id"> <!-- class="col-md-4" -->
+          item
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+var appKey = 'keyrkS74q9vL9FBHT'
+var appId = 'appzdYVnVaVLTKUB7'
+var items = []
+
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    axios.get(
+      'https://api.airtable.com/v0/' + appId + '/Table%201',
+      {
+        headers: { Authorization: 'Bearer ' + appKey }
+      }
+    ).then(function (response) {
+      this.items = response.data.records
+    }.bind(this)).catch(function (error) {
+      console.log(error)
+    })
+  },
+  components: { },
+  data: function () {
+    return data
+  }
 }
 </script>
 
