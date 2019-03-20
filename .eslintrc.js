0e028452289bc918700e6372c1ea01152e423f68
cpasbanal <jcroyere@gmail.com>
2019-03-20 20:43:14 +0100

    eslint allow oneline multi-attributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,14 @@ module.exports = {
  	'plugin:vue/strongly-recommended' // base, essential, strongly-recommended, recommended
  ],
  'rules': {
-   'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-   'vue/v-on-style': 'off' // not working for drag and drop events, v-on is required
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/v-on-style': 'off', // not working for drag and drop events, v-on is required
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3, // allow 3 attributes in a single line tag
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': true
+      }
+   }]
  }
 }
