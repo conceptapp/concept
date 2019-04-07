4063a46468f3c74cf51fcac640b0149edb9a080e
cpasbanal <jcroyere@gmail.com>
2019-04-07 19:26:03 +0200

    Pause now working in timer + clean up code design

--- src/components/timer.vue
+++ src/components/timer.vue
@@ -39,42 +39,44 @@ export default {
   },
   computed: {
     time: function() {
-      return this.hours + ':' + this.minutes + ':' + this.seconds;
+      return this.hours + ':' + this.minutes + ':' + this.seconds
     },
     milliseconds: function() {
-      return this.currentTime - this.$data.startTime;
+      return this.currentTime - this.$data.startTime
     },
     hours: function() {
-      var lapsed = this.milliseconds;
-      var hrs = Math.floor((lapsed / 1000 / 60 / 60));
-      return hrs >= 10 ? hrs : '0' + hrs;
+      var lapsed = this.milliseconds
+      var hrs = Math.floor((lapsed / 1000 / 60 / 60))
+      return hrs >= 10 ? hrs : '0' + hrs
     },
     minutes: function() {
-      var lapsed = this.milliseconds;
-      var min = Math.floor((lapsed / 1000 / 60) % 60);
-      return min >= 10 ? min : '0' + min;
+      var lapsed = this.milliseconds
+      var min = Math.floor((lapsed / 1000 / 60) % 60)
+      return min >= 10 ? min : '0' + min
     },
     seconds: function() {
-      var lapsed = this.milliseconds;
-      var sec = Math.ceil((lapsed / 1000) % 60);
-      return sec >= 10 ? sec : '0' + sec;
+      var lapsed = this.milliseconds
+      var sec = Math.ceil((lapsed / 1000) % 60)
+      return sec >= 10 ? sec : '0' + sec
     }
   },
   methods: {
     reset: function() {
-      this.$data.state = "started";
-      this.$data.startTime = Date.now();
-      this.$data.currentTime = Date.now();
+      this.$data.state = "started"
+      this.$data.startTime = Date.now()
+      this.$data.currentTime = Date.now()
     },
     pause: function() {
       this.$data.state = "paused"
     },
     resume: function() {
       this.$data.state = "started"
+      // shift startTime to take the pause delay into account
+      this.$data.startTime = Date.now() - this.milliseconds
     },
     updateCurrentTime: function() {
       if (this.$data.state == "started") {
-        this.currentTime = Date.now();
+        this.currentTime = Date.now()
       }
     }        
   }
